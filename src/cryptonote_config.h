57833f2bc5fc0c694bb62625565aac595de01479
cryptoinfinitytoken-prog <crypto.infinitytoken@gmail.com>
2025-12-05 04:34:32 -0600

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -15,8 +15,14 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x12, 0x30, 0xF1, 0x71, 0x61, 0x04, 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } };
+  
+  // This is the known-good Monero string. DO NOT CHANGE IT.
   std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbcf74ae0671a93d1d3080948a579ad83121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-  uint32_t const GENESIS_NONCE = 10000;
+  
+  // We change this number to make the coin unique.
+  uint32_t const GENESIS_NONCE = 8888;
+
+  std::string const CRYPTONOTE_NAME = "arcanum";
 }
 
 namespace config
